579d58eacb202dba1e8a088c9c6e94a0be539c79
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-10-18 16:19:54 +0100

    Feature/mttl 1743 #2 (#14)
    * Updated account tenure type
    
    * Update config.yml

---
+++ AssetInformationListener/Domain/Account/AccountTenureType.cs
@@ -0,0 +1,8 @@
+namespace AssetInformationListener.Domain.Account
+{
+    public class AccountTenureType
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
