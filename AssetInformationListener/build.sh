b018b48611d33ee5bff07c12ba8c8b02f2f22d17
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2025-01-31 14:44:29 +0000

    Don't force a specific AWS Lambda Tools version, use latest.

--- AssetInformationListener/build.sh
+++ AssetInformationListener/build.sh
@@ -8,7 +8,7 @@ then
 fi
 
 #dotnet restore
-dotnet tool install --global Amazon.Lambda.Tools --version 5.4.5
+dotnet tool install --global Amazon.Lambda.Tools
 
 
 # (for CI) ensure that the newly-installed tools are on PATH
